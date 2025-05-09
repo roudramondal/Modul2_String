0a464b94e771da2eb5a2913842dd35bd4da7815a
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-09 11:44:25 +0600

    string input

---
+++ string1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  string str;
+  cin >> str;
+  str = "Hello";
+  cout << str << endl;
+}
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  solve();
+  return 0;
+}

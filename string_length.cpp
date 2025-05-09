c5dcbc4ba5088ff18f7a8cb3379d00d70630b2c9
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-09 11:51:14 +0600

    Find the length of a string

---
+++ string_length.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve ()
+{
+  string str;
+  cin >> str;
+  cout << str.length() << endl;
+  //cout << str.size() << endl;//there is no difference between size and length
+}
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  solve();
+  return 0;
+}

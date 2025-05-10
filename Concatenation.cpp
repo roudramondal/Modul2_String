827ad99769a72346ede38f4c4a170ba85ed6bc4a
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-10 23:22:45 +0600

    String Concatenation

---
+++ Concatenation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve ()
+{
+  string s1="Hello";
+  string s2 = " World";
+  string s3 = s1 + s2;
+  cout<<s3<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  solve();
+  return 0;
+}

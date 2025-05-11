f87ad4b79b5c3e54d67a67a1d4398d95a309596b
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-11 16:56:51 +0600

    M2 Find all sub string
    for (int i = 0; i < s1.length();i++)
        {
          for (int j = i; j < s1.length();j++)
          {
            if (s1.substr(i, j - i + 1) ==sub)
            {
         
            }
          }
    
        }

---
+++ print_all_sub_string.cpp
@@ -0,0 +1,65 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 11 / 2025
+   Time  : 16:45
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+#define co cout
+#define c cin
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    string s1;
+    c >> s1;
+    string sub;
+    c >> sub;
+    // for (int i = 0; i < s1.length();i++)
+    // {
+    //   for (int j = i; j < s1.length();j++)
+    //   {
+    //     cout << s1.substr(i, j - i + 1) << nl;
+    //   }
+
+    // }
+    co << "Other Output"<<nl;
+    for (int i = 0; i < s1.length();i++)
+    {
+      for (int j = i; j < s1.length();j++)
+      {
+        if (s1.substr(i, j - i + 1) ==sub)
+        {
+          co << "Got it index= "<<i << nl;
+          return;
+        }
+      }
+
+    }
+    cout << "Not a substr" << nl;
+}
+
+int main() {
+    fast_io;
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

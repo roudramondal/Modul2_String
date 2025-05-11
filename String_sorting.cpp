e2b3a249f24c114615a2092a055c6c77e59b9097
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-11 12:12:28 +0600

    String Sorting (ASCII value)
    sort (string name.begin(), String name.end());

---
+++ String_sorting.cpp
@@ -0,0 +1,41 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 11 / 2025
+   Time  : 12:03
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1.0);
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+void solve() {
+    // Your code here
+    string s ;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << nl; // show output dependent on ASCII value ;
+}
+
+int main() {
+    fast_io;
+     int t;
+     cin >> t;
+    while (t--) {
+        solve();
+     }
+    return 0;
+}
